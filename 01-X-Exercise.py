79241a6392e6dba2b735ee487e506eb4c217e656
Shravan Jha <Shravan.Jha@gmail.com>
2023-08-16 17:00:02 +1000

    exercise 1

---
+++ 01-X-Exercise.py
@@ -0,0 +1,26 @@
+schoolName = "RiverBank Public School"
+schoolAddress = "25 Wentworth St, The Ponds NSW 2769"
+principleName = "Mrs Jeanie Brown"
+myName = "Shravan Jha"
+
+print(f"{schoolName}")
+print(f"{schoolAddress}")
+print(f"Subject: Python Beginner Books Request for School Library")
+print(f"Dear {principleName},")
+
+content = '''
+I'm wri􀆟ng to request Python beginner books for our library. Python's user-friendly approach makes
+it an ideal starting point for coding exploration, providing students with flexible learning, inclusivity,
+and holistic skill development.
+Incorporating these books would:
+1.Enhance Learning: Allow students to explore coding at their own pace, supplementing our
+curriculum.
+2. Ensure Access: Provide technology equality by offering learning resources in physical form.
+3. Promote Skills: Foster cri􀆟cal thinking, creativity, and problem-solving abilities.
+Your consideration of this request is appreciated. If budget concerns arise, I'm available to assist in
+identifying cost-effective op􀆟ons or potential funding sources.
+Thank you for your commitment to a comprehensive education. I look forward to your response.
+Sincerely,
+'''
+print(f"{content}")
+print(f"{myName}")
